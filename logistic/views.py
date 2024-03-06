946d7be5aa18e17019beac76aab0568a94d38f03
addirossi <addirossi@gmail.com>
2024-03-07 00:02:29 +0600

    Change test text

--- logistic/views.py
+++ logistic/views.py
@@ -24,4 +24,4 @@ class StockViewSet(ModelViewSet):
 
 @api_view(['GET'])
 def sample_view(request):
-    return Response({'message': 'Всем привет!'})
+    return Response({'message': 'Это изменённый текст для тестирования!'})
