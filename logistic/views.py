6adaf57999beba4418d8f1b82d2adab8f501ab80
Adilet Asankojoev <addirossi@gmail.com>
2024-04-17 23:54:56 +0600

    Update views.py

--- logistic/views.py
+++ logistic/views.py
@@ -24,4 +24,4 @@ class StockViewSet(ModelViewSet):
 
 @api_view(['GET'])
 def sample_view(request):
-    return Response({'message': 'Измененный текст!'})
+    return Response({'message': 'Всем привет! Всё работает!'})
