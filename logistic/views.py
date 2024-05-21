8e2e20a014095801ea081d126bee433eccd9e5fa
a.asankozhayev <a.asankozhayev@globerce.capital>
2024-05-21 22:55:35 +0600

    Change text

--- logistic/views.py
+++ logistic/views.py
@@ -24,4 +24,4 @@ class StockViewSet(ModelViewSet):
 
 @api_view(['GET'])
 def sample_view(request):
-    return Response({'message': 'Всем привет! Всё работает!'})
+    return Response({'message': 'Hello world!!!!'})
